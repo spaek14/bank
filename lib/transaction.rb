af5bba5e236410ba9c232d4e83bc3d24549ab2bd
spaek14 <>
2021-04-06 18:32:07 +0100

    credit/debit split on transactions

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,17 @@
 class Transaction
+    attr_reader :date, :before_balance, :after_balance, :credit, :debit
 
+    def initialize(before_balance, amount)
+        @date = Time.now.strftime('%d/%m/%y')
+        @before_balance = before_balance
+        @after_balance = before_balance + amount
+        @credit = 0
+        @debit = 0
+        if amount.positive? 
+            @credit += amount
+        elsif amount != 0
+            @debit -= amount
+        else raise "cannot process value of 0"
+        end
+    end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,4 +2,46 @@ require './lib/transaction.rb'
 
 describe Transaction do
 
+    context "has attributes" do
+        it "has a date" do
+            transaction = Transaction.new(before_balance = 0, amount = 10)
+            expect(transaction.date).to eq(Time.now.strftime('%d/%m/%y'))
+        end
+
+        it "has a before balance" do
+            transaction = Transaction.new(before_balance = 0, amount = 10)
+            expect(transaction.before_balance).to eq(0)
+        end
+
+        it "has an after balance" do
+            transaction = Transaction.new(before_balance = 0, amount = 10)
+            expect(transaction.after_balance).to eq(10)
+        end
+    end
+
+    context "amount to credit/debit" do
+        it "if amount is +x, credit = x" do
+            transaction = Transaction.new(before_balance = 0, amount = 10)
+            expect(transaction.credit).to eq(10)
+        end
+
+        it "if amount is -x, debit = x" do
+            transaction = Transaction.new(before_balance = 0, amount = -10)
+            expect(transaction.debit).to eq(10)
+        end
+
+        it "if amount is +x, debit = 0" do
+            transaction = Transaction.new(before_balance = 0, amount = 10)
+            expect(transaction.debit).to eq(0)
+        end
+
+        it "if amount is -x, credit = 0" do
+            transaction = Transaction.new(before_balance = 0, amount = -10)
+            expect(transaction.credit).to eq(0)
+        end
+
+        it "if amount is 0, raise error" do
+            expect { transaction = Transaction.new(before_balance = 0, amount = 0) }.to raise_error"cannot process value of 0"
+        end
+    end
 end
